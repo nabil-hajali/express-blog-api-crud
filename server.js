ab83b90a90185c020cb7cb9d9b1706fa3b6a2ef4
nabil-hajali <nabil2491@gmail.com>
2026-04-09 13:14:34 +0200

    Add middleware for request time checking, error handling, and 404 responses

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 const postsRouter = require('./router/posts');
 const PORT = process.env.PORT || 3001;
+const checkTime = require('./middleware/checkTime');
+const errorHandler = require('./middleware/errorHandler');
+const notFound = require('./middleware/notFound');
 
 app.use(express.json());
-
-
+//register the checkTime middleware
+app.use(checkTime);
+//register the error handler middleware
+app.use(errorHandler);
+//register the not found middleware
+app.use(notFound);
 //register the body-parse 
 app.use('/posts', postsRouter);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server avviato su http://localhost:${PORT}`);
